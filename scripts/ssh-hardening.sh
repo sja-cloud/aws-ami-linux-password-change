e89365b728bc730f3dbc12406ea372f9d3353e9a
sjacloud <shariq.jilani.ahmed@gds.ey.com>
2025-08-04 14:59:25 +0200

    Update ssh-hardening.sh

--- scripts/ssh-hardening.sh
+++ scripts/ssh-hardening.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Updating SSG configuration..."
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentciation no/g' 
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' 
 /etc/ssh/sshd-config
-sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentciation no/g' 
+sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' 
 /etc/ssh/sshd-config
 sudo sed -i 's/#PubKeyAuthentication yes/PubKeyAuthentication yes/g' 
 /etc/ssh/sshd-config
